7e732db6ce7020ac1fb3e71098a99e6c2e2bb489
baeriswyld <daniel@magic-carpet.ch>
2020-03-30 00:31:38 +0100

    pip test

---
+++ example.py
@@ -0,0 +1,36 @@
+from flashmsg import flash_msg
+
+# Example 0
+flash_msg(['Hello World'])
+
+# Example 1
+flash_msg(msg=['Hello World',"I hope you find the flash message useful"],
+          sign='#',
+          bar_length=7,
+          bar_name='Example 1')
+
+# Example 2
+flash_msg(msg=['Very important message',"Clowns = Peter",'Specs: Shoes = 49, Costume=L'],
+          sign="***",
+          bar_length=2,
+          bar_name='Fancy',
+          color='red')
+
+# Example 3
+import random
+import string
+from random import randint
+
+def randomString(stringLength=10):
+    """Generate a random string of fixed length """
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
+
+
+msg = [ randomString(randint(0, 50))  for i in range(10)]
+
+flash_msg(msg=msg,
+          sign="/*-//-*/",
+          bar_length=2,
+          bar_name='Stranger Things',
+          color='green')
